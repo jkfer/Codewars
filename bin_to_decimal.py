75dbd2ed9ea8839daa8185b4c796f63b7639afdc
jkfer <kvin0019@gmail.com>
2019-10-25 17:51:57 -0400

    - old completed

--- bin_to_decimal.py
+++ bin_to_decimal.py
@@ -4,8 +4,12 @@ Complete the function which converts a binary number (given as a string) to a de
 
 from math import pow
 
+
 def bin_to_decimal(inp):
     res = 0
     for i, num in enumerate(inp[::-1]):
         res += int(num) * pow(2, i)
-    return res
+    return int(res)
+
+
+print(bin_to_decimal("100"))
