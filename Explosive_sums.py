9c7fa5006d186fdc766fee169eb06c4e31e9abb4
jkfer <kvin0019@gmail.com>
2019-11-11 20:27:49 -0500

    4kyu

--- Explosive_sums.py
+++ Explosive_sums.py
@@ -1,18 +1,15 @@
 """
 How many ways can you make the sum of a number?
-From wikipedia: https://en.wikipedia.org/wiki/Partition_(number_theory)#
-
-In number theory and combinatorics, a partition of a positive integer n, also called an integer partition, is a way of writing n as a sum of positive integers. Two sums that differ only in the order of their summands are considered the same partition. If order matters, the sum becomes a composition. For example, 4 can be partitioned in five distinct ways:
 
 4
 3 + 1
 2 + 2
 2 + 1 + 1
 1 + 1 + 1 + 1
+
 Examples
-Basic
 exp_sum(1) # 1
-exp_sum(2) # 2  -> 1+1 , 2
+exp_sum(2) # 2 -> 1+1 , 2
 exp_sum(3) # 3 -> 1+1+1, 1+2, 3
 exp_sum(4) # 5 -> 1+1+1+1, 1+1+2, 1+3, 2+2, 4
 exp_sum(5) # 7 -> 1+1+1+1+1, 1+1+1+2, 1+1+3, 1+2+2, 1+4, 5, 2+3
@@ -26,25 +23,34 @@ See here for more examples.
 
 Idea -
 Solve using DP (Dynamic Programming)
-write the number as a combinations of two numbes - then find the ways of making those two numbes from the DP structure:
+write the number as a combinations of two numbes
+then find the ways of making those two numbes from the DP structure:
 
-res = [1, 2]
+0 => 1 (0)
+1 => 1 (1)
+2 => 2 (1, 1), (2)
+3 => 3 (3), (1, 1, 1), (2, 1)
 
+The pattern here is that
+for 1, you add the ways of making 0
+for 2, you add the ways of making, ways(0), ways(1)
+for 3, you add the ways of making, ways(1) + ways(2)
+for 5, you add the ways of making, ways(1) + ways(4) (for 4, you use ways(1), ways(3) for three ways(1), ways(2))
+for 10 you add the ways of making ways(1), ways(9)
 """
-n = 42
+
 
 def exp_sum(n):
-    res = [1, 2]
-    for i in range(2, n + 1):
-        N = i + 1
-        # how many ways you can write N as a sum of two numbers:
-        j = 1
-        count = 0
-        while j <= N // 2:
-            count += res[j] + res[N-j]
-            j += 1
-        
-        res[i] = count
-        print(res)
+    if n in [0, 1, 2]:
+        return n
 
-exp_sum(3)
+    R = [1] + [0] * n
+
+    for i in range(1, n+1):
+        for j in range(i, n+1):
+            R[j] += R[j-i]
+
+    return R[-1]
+
+
+print(exp_sum(10))
